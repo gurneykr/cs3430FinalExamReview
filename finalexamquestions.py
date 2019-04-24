3fe715996145fcd4d5e15198c26842d9a27d71dd
Krista Gurney <gurneykri@gmail.com>
2019-04-24 15:04:34 -0600

    working on pell equations

--- finalexamquestions.py
+++ finalexamquestions.py
@@ -15,12 +15,12 @@ import matplotlib.pyplot as plt
 
 ########## SECTION 3 - THEORY OF THE FIRM #########
 
-#PROBLEM 1
+    #PROBLEM 1
 
 
-#PROBLEM 2
+    #PROBLEM 2
 
-#PROBLEM 3 - demand elasticity f(p) = 100 - 2p
+    #PROBLEM 3 - demand elasticity f(p) = 100 - 2p
 def demand_elasticity(p):
     assert isinstance(p, const)
     demand_eq = make_plus(make_const(100), make_prod(make_const(-2.0), make_pwr('p', 1.0)))
@@ -34,7 +34,7 @@ def demand_elasticity(p):
 
     return num / denom
 
-#PROBLEM 4 - net change
+    #PROBLEM 4 - net change
 def net_change(mrexpr, pl1, pl2):
     assert isinstance(pl1, const)
     assert isinstance(pl2, const)
@@ -46,9 +46,9 @@ def net_change(mrexpr, pl1, pl2):
 
     return F(b.get_val()) - F(a.get_val())
 
-#PROBLEM 5 - consumer surplus - lecture 14
-# f(x)-B evaluated from a to 0
-# where B = f(a)
+    #PROBLEM 5 - consumer surplus - lecture 14
+    # f(x)-B evaluated from a to 0
+    # where B = f(a)
 def consumer_surplus(dexpr, a):
     assert isinstance(a, const)
 
@@ -73,21 +73,112 @@ def consumer_surplus(dexpr, a):
 
 ##### SECTION 6 - ANTIDIFFERENTIATION ############
 
+    ##PROBLEM 2 Riemann sum stuff in defintegralapprox.py
 
 
 ##### SECTION 7 - CURVE FITTING ###########
 
+    ##PROBLEM 1 - Bell curve
+def gaussian_pdf(x, sigma=1, mu=0):
+    a = 1.0 / (sigma * math.sqrt(2 * math.pi))
+    b = math.e ** (-0.5 * (((x - mu) / sigma) ** 2))
+    return a * b
+
+def bell_curve_iq_approx(a, b):
+    assert isinstance(a, const)
+    assert isinstance(b, const)
+
+    iqc = lambda x: gaussian_pdf(x, sigma=16.0, mu=100)
+    print(simpson_rule(iqc, a, b, const(6)))
+
+    #PROBLEM 2 - Method of least squares
+def fit_regression_line(x, y):
+    N = len(x)
+    assert len(y) == N
+    sum_xy = sum(xy[0]* xy[1] for xy in zip(x, y))
+    sum_x = sum(xi for xi in x)
+    sum_y = sum(yi for yi in y)
+    sum_x_sqr = sum(xi**2 for xi in x)
+    A = (1.0*(N*sum_xy - sum_x*sum_y))/(N*sum_x_sqr - sum_x**2)
+    B = (sum_y - A*sum_x)/ (1.0*N)
+    rlf = lambda x: A*x + B
+    return rlf
+
+    #PROBLEM 3 - Taylor polynomials
+def taylor_poly(fexpr, a, n):
+    assert isinstance(a, const)
+    assert isinstance(n, const)
+
+    tof_exp = tof(fexpr)
+    ex = fexpr
+    result = const(tof_exp(a.get_val()))
+
+    for i in range(1, int(n.get_val())):
+        drv = deriv(ex)
+        drv_tof = tof(drv)
+
+        inside = const(drv_tof(a.get_val()) / math.factorial(i))
+        x = make_plus(make_pwr('x', 1.0), make_prod(const(-1.0), a))
+
+        pw = make_pwr_expr(x, i)
+
+        result = make_plus(result, make_prod(inside, pw))
+        ex = drv
+
+    return result
 
 
 ##### SECTION 8 - NEWTON-RAPHSON AND PELL EQUATIONS ######
+    #PROBLEM 1 & 2
+def newton_raphson(poly_fexpr, g, n):
+    tof_expr = tof(poly_fexpr)
+    deriv_expr = tof(deriv(poly_fexpr))
+
+    for i in range(n.get_val()):
+        x = g.get_val() - (tof_expr(g.get_val()) / deriv_expr(g.get_val()))
+        g = const(x)
+
+    return g
+
+    #PROBLEM 4
+# finds x and y that give the solution to
+#the equation x^2 -ky^2 = +- 1
+# def pell(n):
+#     pairs = []
+#
+#     while len(pairs) < n:
+#         for x in range(1, 10000):
+#             for y in range(1, 1000):
+#                 if abs(x**2 - 2*y**2) == 1:
+#                     pairs.append((x,y))
+#
+#     return pairs
+
+def pell(x1, x2, y1, y2):
+    pairs = []
+    f = lambda x,y: x*x - 2*x*y
+
+    for x in range(x1, x2+1):
+        for y in range(y1, y2+1):
+            if abs(f(x,y)) == 1:
+                if (x,y) not in pairs:
+                    pairs.append((x, y))
+    return pairs
 
 
+
+def pell_approx_sqrt(n, a, b):
+    pairs = []
+    for i in range(1, n):
+        x_n = ((a + math.sqrt(n)*b)**n + (a - math.sqrt(n)*b)**n) /2
+        y_n = ((a + math.sqrt(n) * b) ** n + (a - math.sqrt(n) * b) ** n) / (2*math.sqrt(n))
+        pairs.append((x_n, y_n))
+
+
+    return pairs
 
 
 ##### SECTION 9 - LINEAR PROGRAMMING IN 2 VARIABLES #######
-
-
-
 #### SECTION 10 - LINEAR SYSTEMS ###########
 
 
--- test.py
+++ test.py
@@ -3,16 +3,19 @@ from maker import make_line_eq
 from maker import make_var, make_const, make_prod
 from maker import make_pwr, make_plus
 from linprog import line_intersection, get_line_coeffs, maximize_obj_fun, minimize_obj_fun, opt_prob_1a, opt_prob_1b,opt_prob_1c
-from maker import make_point2d
+from maker import make_point2d, make_e_expr
 from const import const
 from var import var
 from prod import prod
 from pwr import pwr
 from plus import plus
 from tof import tof
+from deriv import deriv
 from consts import is_const_line
 import sys
-from finalexamquestions import demand_elasticity, net_change, consumer_surplus
+from finalexamquestions import demand_elasticity, net_change, consumer_surplus, fit_regression_line, taylor_poly, newton_raphson,pell_approx_sqrt, pell
+import numpy as np
+
 
 def test_01():
     print(demand_elasticity(const(10.0)))
@@ -29,6 +32,51 @@ def test_03():
     print(dexpr)
     print(consumer_surplus(dexpr, const(20)))
 
+def test_04():
+    x = np.array([1, 2, 3, 4, 5])
+    y = np.array([2, 3, 4, 5, 6])
+    rlf = fit_regression_line(x, y)
+
+
+    assert rlf(1) == 2.0
+    assert rlf(2) == 3.0
+    assert rlf(3) == 4.0
+    assert rlf(4) == 5.0
+    assert rlf(10) == 11.0
+    assert rlf(101) == 102.0
+    print('Problem 04: Unit Test 01: pass')
+
+def gt21_02(x):
+    ''' ground truth for 2nd taylor of fexpr2_01. '''
+    fexpr2_01 = make_prod(make_pwr('x', 1.0),
+                          make_e_expr(make_pwr('x', 1.0)))
+    f0 = tof(fexpr2_01)
+    f1 = tof(deriv(fexpr2_01))
+    f2 = tof(deriv(deriv(fexpr2_01)))
+    return f0(2.0) + f1(2.0)*(x - 2.0) + (f2(2.0)/2)*(x - 2.0)**2
+
+def test_05():
+    fexpr2_01 = make_prod(make_pwr('x', 1.0),
+                          make_e_expr(make_pwr('x', 1.0)))
+    print(gt21_02(2.001))
+    fex = taylor_poly(fexpr2_01, make_const(2.001), make_const(2))
+    print(fex)
+    fex_tof = tof(fex)
+    print(fex_tof(2.001))
+
+def test_06():
+    ''' Approximating x^3 + x - 1. '''
+    fexpr = make_pwr('x', 3.0)
+    fexpr = make_plus(fexpr, make_pwr('x', 1.0))
+    fexpr = make_plus(fexpr, make_const(-1.0))
+    print(newton_raphson(fexpr, make_const(1.0), make_const(10000)))
+
+def test_07():
+    fexpr = make_plus(make_pwr('x', 2.0), make_const(-3.0))
+    print(newton_raphson(fexpr, make_const(1.0), make_const(10000)))
+
+def test_08():
+    print(pell(0, 1000, 0, 1000))
 
 if __name__ =='__main__':
-    test_03()
+    test_08()
